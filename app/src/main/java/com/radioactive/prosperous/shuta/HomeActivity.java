7c6d8ac76d3e038598cbc22678a0fb7e18cb9527
kyphoenix <kenmariki@gmail.com>
2019-01-17 14:05:40 +0300

    Initial commit

---
+++ app/src/main/java/com/radioactive/prosperous/shuta/HomeActivity.java
@@ -0,0 +1,63 @@
+package com.radioactive.prosperous.shuta;
+
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+public class HomeActivity extends AppCompatActivity {
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    dl =(DrawerLayout)findViewById(R.id.dl);
+    abdt = new ActionBarDrawerToggle(this,dl,R.string.Open,R.string.Close);
+     abdt.setDrawerIndicatorEnabled(true);
+
+
+
+     dl.addDrawerListener(abdt);
+     abdt.syncState();
+
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    NavigationView nav_view = (NavigationView)findViewById(R.id.nav_view);
+     nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            int id = menuItem.getItemId();
+            if (id == R.id.Login){
+                Toast.makeText(HomeActivity.this, "Student'sProfile",Toast.LENGTH_SHORT).show();
+            }
+            else if (id == R.id.Register) {
+                Toast.makeText(HomeActivity.this, "Registered", Toast.LENGTH_SHORT).show();
+            }
+            else if (id == R.id.About) {
+                Toast.makeText(HomeActivity.this, "about", Toast.LENGTH_SHORT).show();
+            }
+            return true;
+        }
+    });
+
+}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (abdt.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
+
